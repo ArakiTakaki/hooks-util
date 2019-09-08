3808835dc6b98571a6cd28638f855720012a96ba
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-09-09 02:26:40 +0900

    uuid系のライブラリ追加

---
+++ src/lib/RandomSeed.ts
@@ -0,0 +1,3 @@
+export default (seed: number = Math.random()) => {
+  return (Math.cos(43758.5453 * Math.sin(43758.5453 * seed) * seed) + 1) / 2
+}
---
+++ src/useAccordion.ts
@@ -0,0 +1,5 @@
+import { useState, useRef } from 'react'
+
+export default (el: HTMLElement) => {
+  el.style.display = 'hidden'
+}
